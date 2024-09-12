5aa6353ba5535af1d31074dbe4c4aab786e1ccf2
SpiNoice01 <fadhilaagil72@gmail.com>
2024-09-13 01:27:22 +0700

    TURU, total 6 Jam belajar. How!?.
    idk i feels smarter now :D

---
+++ RiseUP_W_24sks/2.Mayan/OrAnd.js
@@ -0,0 +1,22 @@
+//  Materi Semester 2 ini, logika pemograman or somtng. Tapi lupa AOKWOWKO
+
+let totalBelanja = 125000;
+let jenisMember = "Bronze";
+
+if (totalBelanja > 100000 && jenisMember == "Silver") {
+  console.log("Selamat, Anda mendapat diskon sebesar 10%");
+} else {
+  console.log("Maaf, saat ini Anda belum mendapat diskon");
+}
+
+console.log("===========================");
+// ===========================================
+
+let totalBelanja2 = 125000;
+let jenisMember2 = "Bronze";
+
+if (totalBelanja2 > 100000 || jenisMember2 == "Silver") {
+  console.log("Selamat, Anda mendapat diskon sebesar 10%");
+} else {
+  console.log("Maaf, saat ini Anda belum mendapat diskon");
+}
